f9c79c4f86dc9b4e41f4b9eb9ebbf43e8bf33c42
xililo <sililosililo82@gmail.com>
2023-06-02 17:53:18 +0200

    Initial Commit

---
+++ bs-confirmation.js
@@ -0,0 +1,32 @@
+$.fn.confirmation = function (options) {
+    var title = options.title || 'Confirmation';
+    var onConfirm = options.onConfirm || function () { };
+
+    $(this).click(function () {
+        var confirmationDialog = $('<div class="popover" role="popover">' +
+            '<div class="popover-arrow"></div>' +
+            '<div class="popover-inner"></div>' +
+            '<div class="d-flex justify-content-between">' +
+            '<span class="btn btn-sm btn-danger w-50">Yes</span>' +
+            '<span class="btn btn-sm btn-secondary w-50">No</span>' +
+            '</div>' +
+            '</div>');
+
+        confirmationDialog.find('.popover-inner').text(options.content || 'Are you sure?');
+        confirmationDialog.find('.btn-primary').click(function () {
+            onConfirm();
+            confirmationDialog.popover('hide');
+        });
+
+        $(this).popover({
+            title: title,
+            content: confirmationDialog,
+            html: true,
+            trigger: 'manual'
+        });
+
+        $(this).popover('show');
+    });
+
+    return this;
+};
